9640263a22b7855103acddf91270fea3c02a1e37
rino <rinovethamoses97@gmail.com>
2018-09-20 19:20:18 +0530

    heroku deployment changes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ mongoose.connection.on('error',function(err){
 });
 app.use(express.static(path.join(__dirname, 'dist')));
 app.get('*',function(req,res){
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, 'client/dist/index.html'));
 });
 app.listen(process.env.PORT||3000,function(){
     console.log('Server running in the port 3000');

f5fad97f6fb904c7dfb59648821889460d569905
rino <rinovethamoses97@gmail.com>
2018-09-20 14:21:39 +0530

    logo added

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var mongoose=require('mongoose');
 var app=express();
 var route=require('./routes/route');
 app.use('/api',route);
-mongoose.connect('mongodb://rino:rino1234@ds161312.mlab.com:61312/a2zblog'); //for Mlab db
-//mongoose.connect('mongodb://localhost:27017/blog'); //for local db
+//mongoose.connect('mongodb://rino:rino1234@ds161312.mlab.com:61312/a2zblog'); //for Mlab db
+mongoose.connect('mongodb://localhost:27017/blog'); //for local db
 mongoose.connection.on('connected',function(){
     console.log("Mongo Db connected");
 });
